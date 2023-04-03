e0f048dbb54a586048764fa66bd97664399f52ca
Jojo <jojotime7x@gmail.com>
2023-04-03 20:52:39 +0100

    added few const lines for server.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const Web3 = require('web3');
+const app = express();
 
 const app = express();
 
