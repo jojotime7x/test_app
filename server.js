5037f1475e2cee08088cfe0245894fa051936fd4
Jojo <jojotime7x@gmail.com>
2023-04-06 03:52:52 +0100

    Index path corrected for style,js, and server.js added path line and redirected

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express = require('express');
+/**const express = require('express');
 const Web3 = require('web3');
 const app = express();
-const path = require('path');
 
 
 
@@ -15,3 +14,26 @@ const server = app.listen(5000);
 const portNumber = server.address().port;
 console.log(`port: ${portNumber}`);
 // can see the port number in terminal - you can dictate the port number
+
+**/
+
+const express = require('express');
+const Web3 = require('web3');
+const app = express();
+const path =require('path')
+
+
+// Serve static files from the public folder
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+})
+
+// serving the index.html file 
+
+const server = app.listen(5000);
+const portNumber = server.address().port;
+console.log(`port: ${portNumber}`);
+
+
