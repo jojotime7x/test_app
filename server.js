f8a158bba5b2fee61bc1b48b1c8ad6abce968c96
Jojo <jojotime7x@gmail.com>
2023-04-06 04:46:54 +0100

    Added infura url for testnetwork

--- server.js
+++ server.js
@@ -21,10 +21,18 @@ const express = require("express");
 const Web3 = require("web3");
 const app = express();
 const path = require("path");
-const web3 = new Web3("https://mainnet.infura.io/v3/shoppe");
+//const web3 = new Web3("https://mainnet.infura.io/v3/shoppe");
+//const mainnet_infura =
+//("https://mainnet.infura.io/v3/1c16ba64e3ff44ac8394b990f027cdd1");
+//const web3 = new web3("miannet_infura");
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://mainnet.infura.io/v3/1c16ba64e3ff44ac8394b990f027cdd1"
+  )
+);
 
 // Serve static files from the public folder
-app.use(express.static(__dirname + "/public"));
+https: app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
